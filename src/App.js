9cf26153f7f21e535f9f993602b2f4d547c5214c
haidarali2017 <haidarcse2017@gmail.com>
2024-11-19 17:33:41 +0600

    assets add and connection indexk page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./pages/About";
+import Award from "./pages/Award";
+import Education from "./pages/Education";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Skill from "./pages/Skill";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/award" element={<Award />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skill" element={<Skill />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
